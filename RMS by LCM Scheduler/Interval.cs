ef70ccd57190d3ca479033c30118bd6a91e503b4
Roark Acord <roark.acord@hotmail.com>
2017-11-06 08:34:59 -0500

    Changes

---
+++ RMS by LCM Scheduler/Interval.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMS_by_LCM_Scheduler
+{
+    class Interval
+    {
+        public int Time { get; set; }
+
+        public double Completed { get; set; }
+
+        public double Missed { get; set; }
+
+        public Interval(int n)
+        {
+            Time = n;
+        }
+    }
+}
